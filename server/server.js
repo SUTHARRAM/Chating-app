0620a476af4f8e0294b914d67371cdb259e9ea7e
SUTHARRAM <rls42847@gmail.com>
2019-06-18 10:09:33 +0530

    Socket added

--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
+const socketIO = require('socket.io'); 
+const http = require('http'); 
 const path = require('path'); 
 const express = require('express'); 
 
 const publicPath = path.join(__dirname, '../public'); 
+const port = process.env.PORT || 3000; 
 
 const app = express(); 
-const port = process.env.PORT || 3000; 
+
+const server = http.createServer(app); 
+const io = socketIO(server); 
+
 app.use(express.static(publicPath)); 
 
+io.on('connection', (socket)=>{
+    console.log('New user connected'); 
+    socket.on('disconnect', ()=>{
+        console.log('User was disconnected'); 
+    }); 
+}); 
 
 
 
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`Server started at port ${port}`); 
 }); 
