5f3302d171f8df800065c87048a3e4bda7aed3bb
SUTHARRAM <rls42847@gmail.com>
2019-06-18 11:14:55 +0530

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -13,9 +13,30 @@ const io = socketIO(server);
 
 app.use(express.static(publicPath)); 
 
-io.on('connection', (socket)=>{
+io.on('connection', function(socket){
     console.log('New user connected'); 
-    socket.on('disconnect', ()=>{
+
+    // socket.emit('newEmail', {
+    //     from: 'rlc@gmail.com',
+    //     text: 'Hey whatshapp',
+    //     createAt: 123
+    // }); 
+
+    // socket.on('createEmail', (newEmail)=>{
+    //     console.log('createEmail', newEmail); 
+    // }); 
+
+    socket.on('createMessage', (message)=>{
+        console.log('message: ', message); 
+    }); 
+
+    socket.emit('newMessage', {
+        from: 'John', 
+        text: 'I am good how are you bro !.',
+        createdAt: 12345
+    }); 
+
+    socket.on('disconnect', function(){
         console.log('User was disconnected'); 
     }); 
 }); 
